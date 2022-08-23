ceb387a969705b6804a3ebbf4a52852973f6088a
99dreada <99dreada@live.co.uk>
2022-08-23 15:35:05 +0100

    Links

--- CALCULATOR/routes.py
+++ CALCULATOR/routes.py
@@ -6,3 +6,15 @@ from CALCULATOR import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/machines')
+def machines():
+    return render_template('machines.html')
+
+@app.route('/materials')
+def materials():
+    return render_template('materials.html')
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
