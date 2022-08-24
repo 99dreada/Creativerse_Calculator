0d43957c5aef5f4e0d92177e52e4685c2fdc1fe3
99dreada <99dreada@live.co.uk>
2022-08-24 11:27:42 +0100

    add db

--- CALCULATOR/__init__.py
+++ CALCULATOR/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
-#from flask.cli import AppGroup
-#import click
-#import os
-#from CALCULATOR.model import db
-#from CALCULATOR.login import login_init_app
+from flask.cli import AppGroup
+import click
+import os
+from CALCULATOR.model import db
 
 app = Flask(__name__)
-"""
+
 def config_db(db_url='sqlite:///db/calculator.db'):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,16 +15,12 @@ def config_db(db_url='sqlite:///db/calculator.db'):
     app.secret_key = "#########"
     db.init_app(app)
 
-def config_login():
-    login_init_app(app)
-
 config_db()
-config_login()
-"""
+
 
 from CALCULATOR import routes
 
-"""
+
 #init the flask commands#
 
 db_cli = AppGroup('db')
@@ -38,4 +33,3 @@ def db_init():
     create_db()
 
 app.cli.add_command(db_cli)
-"""
