9e27bffcd96069cb5abe847131465feba4a37f56
99dreada <99dreada@live.co.uk>
2022-08-26 16:14:43 +0100

    fixed startup.bat

--- startup.bat
+++ startup.bat
@@ -2,64 +2,79 @@
 CLS
 call "venv/Scripts/activate.bat"
 
-:begin
+:menu1
 CLS
 ECHO 1.Full Setup (Run this if first time setup ONLY)
-ECHO 2.Install requirements DNU
-ECHO 3.Build DB DNU
-ECHO 4.Run Application
-ECHO 5.Setup Enviroment DNU
+ECHO 2.Run application options
+ECHO.
+CHOICE /C 12 /M "Enter your choice:"
+
+IF ERRORLEVEL 2 GOTO menu2
+IF ERRORLEVEL 1 GOTO Fullsetup
+
+:menu2
+CLS
+ECHO 1.Run Application
+ECHO 2.Build DB
+ECHO 3.Install requirements.txt
+ECHO 4.Set venv variables
+ECHO 5.Start venv ("run if (venv) isnt at the start of command line")
 ECHO.
 CHOICE /C 12345 /M "Enter your choice:"
 
-IF ERRORLEVEL 5 GOTO Envsetup
-IF ERRORLEVEL 4 GOTO Run
-IF ERRORLEVEL 3 GOTO Builddb
-IF ERRORLEVEL 2 GOTO Install
-IF ERRORLEVEL 1 GOTO Fullsetup
+IF ERRORLEVEL 5 GOTO Startenv
+IF ERRORLEVEL 4 GOTO Envsetup
+IF ERRORLEVEL 3 GOTO Install
+IF ERRORLEVEL 2 GOTO Builddb
+IF ERRORLEVEL 1 GOTO Run
 
-:Envsetup
-ECHO Setting up enviroment
-set FLASK_APP=CALCULATOR
-set FLASK_DEBUG=1
 
 :Fullsetup
 ECHO Installing pip
 py -m pip install --upgrade pip
 ECHO Installing venv
 py -m pip install virtualenv
+python -m pip install --upgrade pip
 ECHO Creating venv
 py -m venv venv
 ECHO Starting enviroment
 .\venv\Scripts\activate
-Echo Installing dependancy
+ECHO Installing dependancy
 pip install -r requirements.txt
 set FLASK_APP=CALCULATOR
 set FLASK_DEBUG=1
-GOTO begin
-
-ECHO Setting up database
+ECHO Setting up DB
 flask db init
-ECHO Setup complete
-pause
-GOTO begin
-
-:Install
-Echo installing CALCULATOR dependancy
-pip install -r requirements.txt
-pause
-GOTO begin
+GOTO menu1
 
 :Builddb
 ECHO Setting up CALCULATOR database
 flask db init
 pause
-GOTO begin
+GOTO menu2
+
+:Install
+Echo installing CALCULATOR dependancy
+pip install -r requirements.txt
+pause
+GOTO menu2
 
 :Run
 Echo Application will now start
 flask run
 if  errorlevel 1 goto ERROR
+
+:Envsetup
+ECHO Setting up enviroment
+set FLASK_APP=CALCULATOR
+set FLASK_DEBUG=1
+pause
+GOTO menu2
+
+:Startenv
+ECHO Starting enviroment
+.\venv\Scripts\activate
+GOTO menu2
 
 :exit
 @exit
